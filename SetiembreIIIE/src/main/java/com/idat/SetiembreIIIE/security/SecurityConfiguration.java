7505c5d8de320d346dff0b8051842b012f953622
Brayan Fernandez <brayanzabaleta59@gmail.com>
2022-10-18 22:01:13 -0500

    seguridad con jwt

--- SetiembreIIIE/src/main/java/com/idat/SetiembreIIIE/security/SecurityConfiguration.java
+++ SetiembreIIIE/src/main/java/com/idat/SetiembreIIIE/security/SecurityConfiguration.java
@@ -1,19 +1,23 @@
 package com.idat.SetiembreIIIE.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -58,21 +62,55 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     //VERSION 2.6.7
 
+    @Autowired
+    private UserDetailService service;
+
+    @Autowired
+    private TokenFilter filter;
+
+    @Autowired
+    private EntryPoint entrypoint;
+
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        // TODO Auto-generated method stub
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        // TODO Auto-generated method stub
+        return super.authenticationManager();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("profesor").password(encoder().encode("123")).roles("ADMIN");
-        auth.inMemoryAuthentication().withUser("estudiante").password(encoder().encode("123")).roles("USER");
+//        auth.inMemoryAuthentication().withUser("profesor").password(encoder().encode("123")).roles("ADMIN");
+//        auth.inMemoryAuthentication().withUser("estudiante").password(encoder().encode("123")).roles("USER");
+        auth.userDetailsService(service).passwordEncoder(encoder());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers(HttpMethod.GET).access("hasAnyRole('USER', 'ADMIN')")
+//                .antMatchers("/api/v1/producto/**").access("hasRole('ADMIN')")
+//                .and()
+//                .httpBasic()
+//                .and()
+//                .csrf().disable();
         http.authorizeRequests()
-                .antMatchers(HttpMethod.GET).access("hasAnyRole('USER', 'ADMIN')")
-                .antMatchers("/api/v1/producto/**").access("hasRole('ADMIN')")
+                .antMatchers("/crearToken").permitAll()
+                .anyRequest()
+                .authenticated()
                 .and()
-                .httpBasic()
+                .exceptionHandling()
+                .authenticationEntryPoint(entrypoint)
                 .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
                 .csrf().disable();
     }
 
